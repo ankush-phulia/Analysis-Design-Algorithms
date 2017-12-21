972f201e4f40b0359771a70ec807882c6658db1b
ankush-phulia <ankush.phulia22@gmail.com>
2017-12-21 14:04:19 +0530

    Tiling problem

---
+++ Assignment 5 - Network Flow/src/Tiling.java
@@ -0,0 +1,172 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Tiling {
+
+	static boolean hasPath(boolean[] visited,ArrayList<HashSet<Integer>> graph, int s, int t, int parent[]){
+//		int n = graph.size();
+		visited[s] = true;
+
+		for (int v:graph.get(s)){
+			if (v==t){
+				parent[v] = s;
+				return true;
+			}
+			else if (!visited[v]){
+//				visited[v] = true;
+				parent[v] = s;
+				if (hasPath(visited,graph,v,t,parent)){
+					return true;
+				}
+			}			
+		}
+		return false;
+//		while (!stack.isEmpty()){
+////			int u = stack.peek();
+////			stack.pop();
+//			for (int v : graph.get(u)){
+//				if (!visited[v]){
+//					stack.push(v);
+//					parent[v] = u;
+//					visited[v] = true;
+//				}
+//				if (v==t){
+//					return true;
+//				}
+//			}
+//		}
+//		return false;
+	}
+	
+	public static int maxFlow(ArrayList<HashSet<Integer>> graph, int s, int t){
+		int n = graph.size();
+		int u,v;
+		int flow = 0;
+		int parent[] = new int[n];
+		boolean[] visited = new boolean[n];
+		while (hasPath(visited,graph,s,t,parent)){
+			for (v = t;v!=s;v = parent[v]){
+				u = parent[v];
+				graph.get(u).remove(v);
+				graph.get(v).add(u);
+				visited[v] = false;
+			}
+			flow++;			
+		}
+		for (int i = 1;i<n;i++){
+			visited[i] = false;
+		}
+		while (hasPath(visited,graph,s,t,parent)){
+			for (v = t;v!=s;v = parent[v]){
+				u = parent[v];
+				graph.get(u).remove(v);
+				graph.get(v).add(u);
+				visited[v] = false;
+			}
+			flow++;			
+		}
+		
+		return flow;
+	}	
+	
+	public static void main(String[] args) throws IOException {
+		
+		File input_file = new File("input.txt");
+		Scanner s = new Scanner(input_file);
+		
+		int n = Integer.parseInt(s.nextLine());
+		boolean[][] grid = new boolean[n][n];
+		int cnt1 = 0;
+		int cnt2 = 0;
+		for (int i = 0;i<n;i++){
+			String row = s.nextLine();
+			for (int j = 0;j<n;j++){
+				grid[i][j] = (row.charAt(j)=='1');
+				if (!grid[i][j]){
+					if ((i+j)%2==0){
+						cnt1++;
+					}
+					else{
+						cnt2++;
+					}
+				}
+			}
+		}	
+		s.close();
+		
+		ArrayList<HashSet<Integer>> graph = new ArrayList<HashSet<Integer>>(n*n + 2);
+		HashSet<Integer> source = new HashSet<Integer>();
+		graph.add(source);
+
+		for (int i = 0;i<n;i++){
+			for (int j = 0;j<n;j++){
+				HashSet<Integer> adj = new HashSet<Integer>();
+				if (grid[i][j]){
+					if ((i+j)%2 == 0){ 
+						graph.get(0).add(n*i+j+1); //from source
+						if (i>0 && grid[i-1][j]){ //just above
+							adj.add(1+ n*(i-1) + j);
+						}
+						if (i<n-1 && grid[i+1][j]){ //just below
+							adj.add(1+ n*(i+1) + j);
+						}
+						if (j>0 && grid[i][j-1]){ //just left
+							adj.add(n*(i) + j);
+						}
+						if (j<n-1 && grid[i][j+1]){ //just right
+							adj.add(n*(i) + j + 2);
+						}
+					}
+					else{ //to sink
+						adj.add(n*n+1);
+					}
+				}
+				graph.add(adj);
+			}
+		}
+		HashSet<Integer> sink = new HashSet<Integer>();
+		graph.add(sink);
+		String out = "";
+
+		if (cnt1-cnt2!=0){
+			out = "0";
+		}
+		else{
+			long time = System.currentTimeMillis();
+			int flow = (maxFlow(graph,0,n*n+1));
+			System.out.println(flow);
+			if (flow == (n*n-cnt1-cnt2)/2){
+				out += "1\n";
+				int x,y,xx,yy;
+				int adj = 0;			
+				for (int i = 1;i<n*n;i++){
+					x = (i-1)%n+1;
+					y = (i-1)/n+1;
+					if ((x+y)%2==1 && !graph.get(i).isEmpty()){
+						for (int a:graph.get(i)){
+							adj = a;
+							break;
+						}
+						xx = ((adj-1)%n+1);
+						yy = ((adj-1)/n+1);
+
+						out += ("("+x+","+y+")"+"("+xx+","+yy+")\n");
+					}
+				}
+			}
+			else{
+				out = "0";
+			}			
+			System.out.println("Time taken : " + (System.currentTimeMillis()-time));
+		}
+				
+		FileWriter fwrite = new FileWriter("output.txt");		
+		fwrite.write(out);
+		fwrite.close();
+	}
+
+}
---
+++ Assignment 5 - Network Flow/src/check.java
@@ -0,0 +1,49 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class check {
+	
+	public static void main(String[] args) throws IOException {
+		
+		File input_file = new File("input.txt");
+		Scanner s = new Scanner(input_file);
+		
+		int n = Integer.parseInt(s.nextLine());
+		int[][] grid = new int[n][n];
+		int cnt1 = 0;
+		int cnt2 = 0;
+		for (int i = 0;i<n;i++){
+			String row = s.nextLine();
+			for (int j = 0;j<n;j++){
+				grid[i][j] = (row.charAt(j) - '0');
+			}
+		}	
+		s.close();
+
+		input_file = new File("output.txt");
+		s = new Scanner(input_file);
+		s.nextLine();
+		while (s.hasNextLine()){
+			String str = s.nextLine();
+			if (!s.equals("") || !s.equals(" ")){
+				String[] tile = str.split("[(),]");
+				// for (int i = 0;i<6;i++){
+				// 	System.out.println("yo" + tile[i]);
+				// }
+				grid[Integer.parseInt(tile[1])-1][Integer.parseInt(tile[2])-1]++;
+				grid[Integer.parseInt(tile[4])-1][Integer.parseInt(tile[5])-1]++;
+			}
+		}
+		s.close();
+
+		for (int i = 0;i<n;i++){
+			for (int j = 0;j<n;j++){
+				System.out.print(grid[i][j]);
+			}
+			System.out.println();
+		}	
+
+	}
+
+}
