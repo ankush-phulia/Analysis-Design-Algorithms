c9b61776eb3bd8ba6d97a73d4139523a8ab4b229
ankush-phulia <ankush.phulia22@gmail.com>
2017-12-21 13:58:42 +0530

    Vertices with even length path from source

---
+++ Assignment 1 - Basic Graph Algorithms/src/Graph.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Graph {
+	
+	int V;
+	HashMap<Integer,ArrayList<Integer>> adjList;
+
+	public Graph(){
+		this.V = 0;
+		this.adjList = new HashMap<Integer,ArrayList<Integer>>();
+	}
+	
+	public void addVertex(Integer v){
+		this.V ++;
+		this.adjList.put(v, new ArrayList<Integer>());
+	}
+	
+	public boolean searchVertex(Integer v){
+		return this.adjList.containsKey(v);
+	}
+	
+	public void addEdge(Integer v1, Integer v2){
+		if (searchVertex(v1)){
+			this.adjList.get(v1).add(v2);
+		}
+		else{
+			System.out.println("Vertex "+v1+" not present in the Graph");
+		}
+	}
+
+}
